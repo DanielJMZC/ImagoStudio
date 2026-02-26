81e0f39e43b7a1bab60f563d2e53a14094a8d62a
ItsKylos <104187178+ItsKylos@users.noreply.github.com>
2026-02-26 12:47:37 -0600

    Proyecto Inicial Asp.Net Core
    Proyecto Inicial Asp.Net Core

---
+++ AWAQTrainingGrounds/AWAQTrainingGrounds/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AWAQTrainingGrounds.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
